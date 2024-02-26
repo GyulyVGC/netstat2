fb892d54e3c2ac2454f4bd93c8e5708db9742baa
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-02-26 14:38:03 +0100

    use linux module for all systems that are not macos, ios, or windows

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -1,4 +1,4 @@
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(all(not(target_os = "macos"), not(target_os = "ios"), not(target_os = "windows")))]
 mod linux;
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 mod osx;
@@ -8,7 +8,7 @@ mod windows;
 mod shared_api;
 pub use self::shared_api::*;
 
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(all(not(target_os = "macos"), not(target_os = "ios"), not(target_os = "windows")))]
 pub use self::linux::*;
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 pub use self::osx::*;
